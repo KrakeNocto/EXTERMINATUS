585002f517d391064247417b54d8b3c390ed83cc
KrakeNocto <octo2kraken@gmail.com>
2024-12-13 22:23:03 +0300

    Update ULTIMATE_DESTRUCTION.sh

--- ULTIMATE_DESTRUCTION.sh
+++ ULTIMATE_DESTRUCTION.sh
@@ -1,5 +1,5 @@
-systemctl stop xiond story-testnet sided fueld side nilliond allorad story-testnet-geth story_noded story-gethd morph morph_gethd nibutlightd
-systemctl disable xiond story-testnet sided fueld side nilliond allorad story-testnet-geth story_noded story-gethd morph morph_gethd nibutlightd
+systemctl stop xiond story-testnet sided fueld side nilliond story-testnet-geth story_noded story-gethd morph morph_gethd nibutlightd
+systemctl disable xiond story-testnet sided fueld side nilliond story-testnet-geth story_noded story-gethd morph morph_gethd nibutlightd
 docker stop nwaku-compose-nwaku-1 && docker rm nwaku-compose-nwaku-1
 docker stop worker && docker rm worker
 docker stop inference-basic-eth-pred && docker rm inference-basic-eth-pred
@@ -10,7 +10,6 @@ rm -rf .sided
 rm -rf .xiond
 rm -rf .story
 rm -rf .nillionapp
-rm -rf .allorad
 rm -rf basic-coin-prediction-node
 rm -rf /home/ritual/.morph
 rm -rf /home/ritual/.nillionapp
